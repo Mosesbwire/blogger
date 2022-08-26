71e62e6363da0d40ea98746f5349b4a28f53bc8c
musa1992 <mosesdev92@gmail.com>
2022-08-26 12:36:29 +0300

    remove comment subschema from article model, replace with an array of comment ids, to avoid document exceeding size limit if comments and replies become too many

--- models/articleModel.js
+++ models/articleModel.js
@@ -1,25 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const CommentSchema = new Schema({
-    commentor: {type: Schema.Types.ObjectId, required: true},
-    comment: {type: String, required: true},
-    date: {type: Date, default: Date.now()},
-    replies: [{
-        commentor: {type: Schema.Types.ObjectId, required: true},
-        comment: {type: String, required: true},
-        date: {type: Date, default: Date.now()},
-    }]
-})
-
 const ArticleSchema = new Schema({
-    author: {type: Schema.Types.ObjectId, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Author',required: true},
     publishDate: {type: Date, required: true, default: Date.now()},
     title: {type: String,required: true},
     body: {type: String, required: true},
     updated: {type: Boolean, default: false},
     updateDate: {type: Date},
-    comments: [CommentSchema]
+    comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
 })
 
 module.exports = mongoose.model('Article', ArticleSchema)
