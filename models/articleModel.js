6340be306b76b474dab19073abe6b610313b1950
musa1992 <mosesdev92@gmail.com>
2022-08-26 11:30:39 +0300

    add comment sub-schema to article model

--- models/articleModel.js
+++ models/articleModel.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const CommentSchema = new Schema({
+    commentor: {type: Schema.Types.ObjectId, required: true},
+    comment: {type: String, required: true},
+    date: {type: Date, default: Date.now()},
+    replies: [{
+        commentor: {type: Schema.Types.ObjectId, required: true},
+        comment: {type: String, required: true},
+        date: {type: Date, default: Date.now()},
+    }]
+})
+
 const ArticleSchema = new Schema({
     author: {type: Schema.Types.ObjectId, required: true},
     publishDate: {type: Date, required: true, default: Date.now()},
     title: {type: String,required: true},
     body: {type: String, required: true},
     updated: {type: Boolean, default: false},
-    updateDate: {type: Date}
+    updateDate: {type: Date},
+    comments: [CommentSchema]
 })
 
 module.exports = mongoose.model('Article', ArticleSchema)
