1b1b95b3ed4118f238bfca1e559afa9eaddbb098
musa1992 <mosesdev92@gmail.com>
2022-08-30 14:57:26 +0300

    add fields to hold users followers and followings. Add functions to add followers and users following

--- models/authorModel.js
+++ models/authorModel.js
@@ -9,18 +9,51 @@ const AuthorSchema = new Schema({
     lastname: {type: String, minLength: 2,maxLength: 100, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    profileImage: {type: String, default: 'default'}
+    profileImage: {type: String, default: 'default'},
+    following: [{type: Schema.Types.ObjectId, ref: 'Author'}],
+    followers: [{type: Schema.Types.ObjectId, ref: 'Author'}],
+    followers_count: {type: Number, default: 0},
+    following_count: {type: Number, default: 0},
+
 })
 
 
 AuthorSchema.methods.generateHash = async function(password){
+
     const hashedPassword = await bcrypt.hash(password, saltRounds)
     return hashedPassword
 }
 
 AuthorSchema.methods.validPassword = async function(password){
+    
     return await bcrypt.compare(password, this.password)
 }
+
+AuthorSchema.methods.follow = async function(user){
+    this.following.push(user._id)
+    this.following_count += 1
+    this.save(err=>{
+        if(err){ return new Error({message: 'Save failed'})}
+
+        return {
+            following: this.following_count,
+            user: user
+        }
+    })
+} 
+
+AuthorSchema.methods.gainFollower = async function(follower){
+   
+    this.followers.push(follower._id)
+    this.followers_count += 1
+    this.save((err)=>{
+        if(err){ return new Error({message: 'Save failed'})}
+
+        return follower
+    })
+}
+
+
 
 
 
